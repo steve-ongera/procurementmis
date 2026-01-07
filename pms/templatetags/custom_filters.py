203f406b9215bb619e60ad0da1a1533c27c9e5ba
steve-ongera <gadafisteve001@gmail.com>
2026-01-07 20:28:36 +0300

    made updates

--- pms/templatetags/custom_filters.py
+++ pms/templatetags/custom_filters.py
@@ -7,3 +7,11 @@ def get_item(dictionary, key):
     if dictionary:
         return dictionary.get(key, '')
     return ''
+
+
+@register.filter(name='abs')
+def absolute_value(value):
+    try:
+        return abs(value)
+    except (TypeError, ValueError):
+        return value
